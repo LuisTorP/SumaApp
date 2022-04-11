c12fdce71782f2c89ced2ec8efb646397394b539
LuisTorP <72195998@continental.edu.pe>
2022-04-11 16:50:45 -0500

    Se implemento y probo operación resta test_operacionResta_dosNumerosPositivos_retornaResta

--- tests/PruebaSuma.py
+++ tests/PruebaSuma.py
@@ -10,12 +10,30 @@ class PruebaSuma(unittest.TestCase):
     def test_operacionSuma_dosNumerosPositivos_retornaSuma(self):
         # Arrange
         self.suma=Suma()
-        self.Sumando1=19
-        self.Sumando2=50
+        self.Minuendo=19
+        self.sustraendo=50
         self.resultadoEsperado=69
 
         # Do
-        self.resultadoActual=self.suma.operacionSuma(self.Sumando1,self.Sumando2)
+        self.resultadoActual=self.suma.operacionSuma(self.Minuendo, self.sustraendo)
+
+        # Assert
+        self.assertEqual(self.resultadoEsperado,self.resultadoActual)
+
+class PruebaResta (unittest.TestCase):
+    def setUp(self):
+        self.resta = Suma ()
+
+    def tearDown(self):
+        self.resta = None
+    def test_operacionResta_dosNumerosPositivos_retornaResta(self):
+        # Arrange
+        self.minuendo=19
+        self.sustraendo=2
+        self.resultadoEsperado=17
+
+        # Do
+        self.resultadoActual=self.suma.operacionResta(self.minuendo, self.sustraendo)
 
         # Assert
         self.assertEqual(self.resultadoEsperado,self.resultadoActual)
